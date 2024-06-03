37d3235e74575e0abd2b4d97878f4da74295a6f3
Andraina Rm <andrainaramaro@gmail.com>
2024-06-03 23:08:36 +0300

    feat added ModelView classes

---
+++ org/framework/MappingNotFoundException.java
@@ -0,0 +1,11 @@
+package org.framework.exceptions;
+
+public class MappingNotFoundException extends Exception {
+    public MappingNotFoundException() {
+        super();
+    }
+
+    public MappingNotFoundException(String message) {
+        super(message);
+    }
+}
