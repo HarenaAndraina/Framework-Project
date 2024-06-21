a1b804422531596076b3ce4bd427e9ea1cbd9f11
Andraina Rm <andrainaramaro@gmail.com>
2024-06-21 13:42:23 +0300

    feat added object parameter

---
+++ org/framework/FieldParamNameChecker.java
@@ -0,0 +1,11 @@
+package org.framework.checker;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FieldParamNameChecker {
+    
+}
--- org/framework/ViewScan.java
+++ org/framework/ViewScan.java
@@ -1,24 +1,33 @@
 package org.framework.viewScan;
 
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import org.framework.checker.FieldParamNameChecker;
 import org.framework.checker.Mapping;
 import org.framework.checker.ParamChecker;
 import org.framework.checker.RequestMappingChecker;
 import org.framework.view.ModelView;
 import org.framework.view.RedirectView;
+import org.framework.checker.ParamChecker.ParamWithType;
+import org.framework.annotation.FieldParamName;
 import org.framework.annotation.Param;
 import org.framework.exceptions.InvocationMethodException;
 import org.framework.exceptions.MappingNotFoundException;
 import org.framework.exceptions.ViewException;
+
+import com.thoughtworks.paranamer.AdaptiveParanamer;
+import com.thoughtworks.paranamer.Paranamer;
 
 /**
  * ModelAndView
@@ -136,38 +145,101 @@ public class ViewScan {
                 result = method.invoke(instance);
             } else {
                 Parameter[] parameters = method.getParameters();
+                Class<?>[] parameterTypes = new Class[parameterCount];
+                for (int i = 0; i < parameterTypes.length; i++) {
+                    parameterTypes[i] = parameters[i].getType();
+                }
+
+                Method method1 = clazz.getMethod(methodName, parameterTypes);
+                Paranamer paranamer = new AdaptiveParanamer();
+                String[] parameterNames = new String[parameterCount];
+
+                try {
+                    parameterNames = paranamer.lookupParameterNames(method1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new InvocationMethodException("There is some trouble in paranamer");
+                }
+
                 Object[] args = new Object[parameterCount];
-                
                 ParamChecker checkerAnnotationParam = new ParamChecker();
                 checkerAnnotationParam.getAllMethodParam(parameters);
-                List<Param> listParam = checkerAnnotationParam.getParamList();
+                List<ParamWithType> listParam = checkerAnnotationParam.getParamList();
 
                 for (int i = 0; i < args.length; i++) {
                     if (listParam.size() == 0) {
-                        if (parameters[i].getType() == String.class) {
-                            Parameter parameter = parameters[i];
-                            String paramName = parameter.getName();
-                            
-                            addArgs(request,args,i,paramName);
-                        } 
-                    }else {
-                        String paramName = listParam.get(i).value();
-                        addArgs(request, args, i, paramName);
+                        Parameter parameter = parameters[i];
+                        String paramName = parameterNames[i];
+                        if (parameter.getType() == String.class) {
+                            addArgsString(request, args, i, paramName);
+                        } else {
+                            Class<?> paramClass = parameter.getType();
+                            try {
+                                addArgsObject(request, args, i, paramName, paramClass);
+                            } catch (InvocationMethodException e) {
+                                throw e;
+                            }
+                        }
+                    } else {
+                        String paramName = listParam.get(i).getParam().value();
+                        if (!listParam.get(i).getType().equals(String.class)) {
+                            Class<?> paramClass = listParam.get(i).getType();
+                            try {
+                                addArgsObject(request, args, i, paramName, paramClass);
+                            } catch (InvocationMethodException e) {
+                                throw e;
+                            }
+                        } else {
+                            addArgsString(request, args, i, paramName);
+                        }
                     }
                     result = method.invoke(instance, args);
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
-            throw new InvocationMethodException("There is some error invoking the method of this url");
+            throw new InvocationMethodException(
+                    "There is some error invoking the method of this url: " + e.getMessage());
         }
         return result;
     }
 
-    private static void addArgs(HttpServletRequest request, Object[] args, int i, String paramName) {
+    private static void addArgsObject(HttpServletRequest request, Object[] args, int i, String paramName,
+            Class<?> paramClass) throws InvocationMethodException {
+        try {
+            Object paramObject = paramClass.getDeclaredConstructor().newInstance();
+            Field[] fields = paramClass.getDeclaredFields();
+
+            for (Field field2 : fields) {
+                field2.setAccessible(true);
+                if (field2.isAnnotationPresent(FieldParamName.class)) {
+                    FieldParamName fieldParamName = field2.getAnnotation(FieldParamName.class);
+
+                    String fieldName = fieldParamName.value();
+                    String fullParamName = paramName + "." + fieldName;
+                    String paramValue = request.getParameter(fullParamName);
+
+                    if (paramValue != null) {
+                        Object convertedValue = convertToFieldType(field2, paramValue);
+                        field2.set(paramObject, convertedValue);
+                    }
+                }
+            }
+
+            args[i] = paramObject;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new InvocationMethodException("Cannot access the field parameter");
+        }
+    }
+
+    private static void addArgsString(HttpServletRequest request, Object[] args, int i, String paramName) {
         String requestParam = request.getParameter(paramName);
-        System.out.println("param name: "+paramName);
-        System.out.println("value: "+ requestParam);
+
+        System.out.println("param name: " + paramName);
+        System.out.println("value: " + requestParam);
+        // mila caste'na aminy loniny
+
         if (requestParam != null) {
             args[i] = requestParam;
         } else {
@@ -175,4 +247,23 @@ public class ViewScan {
         }
     }
 
+    private static Object convertToFieldType(Field field, String value) throws Exception {
+        Class<?> fieldType = field.getType();
+
+        if (fieldType.equals(int.class) || fieldType.equals(Integer.class)) {
+            return Integer.parseInt(value);
+        } else if (fieldType.equals(long.class) || fieldType.equals(Long.class)) {
+            return Long.parseLong(value);
+        } else if (fieldType.equals(float.class) || fieldType.equals(Float.class)) {
+            return Float.parseFloat(value);
+        } else if (fieldType.equals(double.class) || fieldType.equals(Double.class)) {
+            return Double.parseDouble(value);
+        } else if (fieldType.equals(boolean.class) || fieldType.equals(Boolean.class)) {
+            return Boolean.parseBoolean(value);
+        } else if (fieldType.equals(String.class)) {
+            return value;
+        } else {
+            throw new IllegalArgumentException("Unsupported field type: " + fieldType);
+        }
+    }
 }
